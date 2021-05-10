76d57aa56a4cb2028cd15b91aefdb134d2de0e01
banerjeeakash97 <50148648+banerjeeakash97@users.noreply.github.com>
2021-05-10 23:09:38 +0530

    Update test_add_fish_to_aquarium.py

--- test_add_fish_to_aquarium.py
+++ test_add_fish_to_aquarium.py
@@ -4,5 +4,5 @@ import add_fish
 class TestAddFishToAquarium(unittest.TestCase):
     def test_add_fish_to_aquarium_success(self):
         actual = add_fish_to_aquarium(fish_list=["shark", "tuna"])
-        expected = {"tank_a": ["rabbit"]}
+        expected = {"tank_a": ["shark", "tuna"]}
         self.assertEqual(actual, expected)
