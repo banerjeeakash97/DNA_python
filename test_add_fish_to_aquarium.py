1032e9d6ae993479556b3abb94dfe99eee3ffca7
banerjeeakash97 <50148648+banerjeeakash97@users.noreply.github.com>
2021-05-10 23:06:29 +0530

    Update test_add_fish_to_aquarium.py

--- test_add_fish_to_aquarium.py
+++ test_add_fish_to_aquarium.py
@@ -1,10 +1,5 @@
 import unittest
-
-def add_fish_to_aquarium(fish_list):
-    if len(fish_list) > 10:
-        raise ValueError("A maximum of 10 fish can be added to the aquarium")
-    return {"tank_a": fish_list}
-
+import add_fish_to_aquarium from add_fish
 
 class TestAddFishToAquarium(unittest.TestCase):
     def test_add_fish_to_aquarium_success(self):
